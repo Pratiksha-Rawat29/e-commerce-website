7002f11a863e72d9222eee3b8eb869592ceafacf
Pratiksha-Rawat29 <pratiksharawat214@gmail.com>
2026-09-24 22:29:38 +0530

    Clear PayPal client_id and client_secret
    Remove sensitive PayPal credentials from configuration.

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "ARw1GANk5UsdSV3zzPGB1dMTcD05l7I4DALTG6dAh1_VAzXc8vwSlEGZqoNIzl__INIobGRLAcX1kn8r",
-  client_secret: "EOnFZNEgAmuMiuXMOdSxHGHoFZX6JR9AV4u6h27wOyjQvW7mxBVTMqzUV2W6M4Lfwiucul6ppPrGUcrg",
+  client_id: "",
+  client_secret: "",
 });
 
 module.exports = paypal;
