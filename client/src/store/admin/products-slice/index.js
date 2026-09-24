15bb53b7525ce302c58448d618ee00a808e317aa
pratiksha <pratiksharawat214@gmail.com>
2026-09-24 20:14:04 +0530

    Configure frontend API URL for deployment

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -10,7 +10,7 @@ export const addNewProduct = createAsyncThunk(
   "/products/addnewproduct",
   async (formData) => {
     const result = await axios.post(
-      "http://localhost:5001/api/admin/products/add",
+      import.meta.env.VITE_API_URL + "/api/admin/products/add",
       formData,
       {
         headers: {
@@ -27,7 +27,7 @@ export const fetchAllProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async () => {
     const result = await axios.get(
-      "http://localhost:5001/api/admin/products/get"
+      import.meta.env.VITE_API_URL + "/api/admin/products/get"
     );
 
     return result?.data;
